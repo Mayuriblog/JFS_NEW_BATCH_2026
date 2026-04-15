f7d211de224227ee1d63da82c006fa2b7c2b9875
unknown <k.mayuri49@gmail.com>
2026-04-15 16:27:02 +0530

    up to date

---
+++ IVSEM/TMap_AllOps.java
@@ -0,0 +1,36 @@
+/*
+import java.util.*;
+
+class TMap_AllOps {
+    public static void main(String[] args) {
+
+        TreeMap<Integer, String> tmap =new TreeMap<>(Collections.reverseOrder());
+
+        tmap.put(1, "A");
+        tmap.put(3, "C");
+        tmap.put(2, "B");
+
+        System.out.println(tmap);
+    }
+}
+*/
+import java.util.*;
+class MyComparator implements Comparator<Integer>
+{
+    public int compare(Integer a, Integer b) 
+	{
+        return b - a;  // descending
+    }
+}
+
+class Test 
+{
+    public static void main(String[] args)
+	{
+		TreeMap<Integer, String> tmap =new TreeMap<>(new MyComparator());
+        tmap.put(1, "A");
+        tmap.put(3, "C");
+        tmap.put(2, "B");
+        System.out.println(tmap);
+    }
+}
